a1bca49c435a56345a654de2ebded56bb83fd6a1
Huzaif Mushtaq <huzaaifmushtaq@gmail.com>
2025-11-11 21:45:55 +0530

    feat: NEW user-facing features only; refactor: code improvements, restructuring, internal changes;

--- backend/routes/jobRoutes.js
+++ backend/routes/jobRoutes.js
@@ -1,19 +1,15 @@
-const express = require('express')
-const jobController = require('./../controller/jobController')
-const authController = require('./../controller/authController')
-const router = express.Router()
+const express = require("express");
+const jobController = require("./../controller/jobController");
+const authController = require("./../controller/authController");
+const router = express.Router();
 
-
-router.post('/create', authController.protect, jobController.createjob)
-router
-      .route('/')
-      .get(authController.protect, jobController.getAllJobs)
-
+router.post("/create", authController.protect, jobController.createjob);
+router.route("/").get(authController.protect, jobController.getAllJobs);
 
 router
-      .route('/:id')
-      .get(authController.protect, jobController.getJob)
-      .patch(authController.protect, jobController.updateJob)
-      .delete(authController.protect, jobController.deleteJob)
+  .route("/:id")
+  .get(authController.protect, jobController.getJob)
+  .patch(authController.protect, jobController.updateJob)
+  .delete(authController.protect, jobController.deleteJob);
 
-module.exports = router
+module.exports = router;
